45714ebca45d602e29cea03b71435f9d7f1c5194
eliseevalex20 <64779288+eliseevalex20@users.noreply.github.com>
2020-08-11 18:58:44 +0300

    anchores

---
+++ js/anchors.js
@@ -0,0 +1,13 @@
+const anchors = document.querySelectorAll('a.up')
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault()
+      
+      const blockID = anchor.getAttribute('href')
+      
+      document.querySelector(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
+    })
+  }
